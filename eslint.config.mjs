1070a9c507b7a1fc861849d4b74bd0c59e307046
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-08-26 13:16:02 +0300

    init commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,37 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      // Variables
+      'no-unused-vars': 'warn',
+      'no-undef': 'off',
+      'no-use-before-define': 'error',
+      'no-shadow': 'warn',
+
+      // Code quality
+      eqeqeq: 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-duplicate-case': 'error',
+      'no-empty': 'warn',
+      curly: 'error',
+
+      // Console
+      'no-console': 'warn',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
